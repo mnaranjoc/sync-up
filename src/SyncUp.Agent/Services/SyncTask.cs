4b16e8961b558dc7c8eeb580779ec51e8084b287
Manuel Naranjo <manuelnaranjo@outlook.com>
2026-07-09 18:59:39 -0600

    agent: sync tasks

--- src/SyncUp.Agent/Services/SyncTask.cs
+++ src/SyncUp.Agent/Services/SyncTask.cs
@@ -1,4 +1,5 @@
 ﻿using SyncUp.Agent.Services.SyncUpService;
+using SyncUp.Shared.Models;
 using System.Text.Json;
 
 namespace SyncUp.Agent.Services
@@ -20,12 +21,23 @@ namespace SyncUp.Agent.Services
             {
                 _logger.LogInformation("SyncTask running at: {time}", DateTimeOffset.Now);
 
+                // Get server files
                 var serverFiles = await _service.GetServerFilesList();
                 if (serverFiles != null)
                 {
-                    var serverFilesJson = JsonSerializer.Serialize(serverFiles);
-                    _logger.LogInformation(serverFilesJson);
+                    _logger.LogInformation("Server has {Count} files.", serverFiles.Count);
                 }
+
+                // Get agent files
+                var agentFiles = await _service.GetAgentFilesList();
+                if (agentFiles != null)
+                {
+                    _logger.LogInformation("Agent has {Count} files.", agentFiles.Count);
+                }
+
+                // Generate download list
+
+                // Generate upload list
 
                 await Task.Delay(5000, stoppingToken);
             }
